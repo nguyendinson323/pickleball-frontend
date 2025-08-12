a9495633800a7efd5eba514d55084791ad50665a
nguyendinson323 <nguyendinson323@gmail.com>
2025-08-12 15:56:23 -0500

    changed baseURL into railway

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,2 +1,2 @@
-// export const API_URL = 'http://localhost:5000/api/v1';
-export const baseURL = 'http://localhost:5000/api/v1';
+export const baseURL = 'https://pickleball-backend.railway.internal/api/v1';
+// export const baseURL = 'http://localhost:5000/api/v1';
