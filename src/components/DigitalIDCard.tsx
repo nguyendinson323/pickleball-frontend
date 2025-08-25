c8177b21aab5257b854149943d9b7ca89e31b53b
nguyendinson323 <nguyendinson323@gmail.com>
2025-08-25 07:37:31 -0500

    completed digital credential system

--- src/components/DigitalIDCard.tsx
+++ src/components/DigitalIDCard.tsx
@@ -359,13 +359,13 @@ const DigitalIDCard: React.FC<DigitalIDCardProps> = ({ className = '' }) => {
               Print
             </button>
 
-            <button 
+            {/* <button 
               onClick={handleShare}
               className="px-4 py-2 border border-gray-300 rounded-md text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 flex items-center"
             >
               <Share2 className="h-4 w-4 mr-2" />
               Share
-            </button>
+            </button> */}
 
             <button 
               onClick={() => setShowFullScreen(true)}
